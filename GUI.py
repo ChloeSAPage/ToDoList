a11c06bca14db24b4147511ffb0ad4b11e2ee85b
ChloeSPage <chloe.s.a.page@Gmail.com>
2023-06-25 19:58:13 +0100

    Basic GUI without interactions

--- GUI.py
+++ GUI.py
@@ -1,2 +1,10 @@
 import Functions
-import PySimpleGUI 
+import PySimpleGUI as sg
+
+label = sg.Text("Type in a Todo")
+input_box = sg.InputText(tooltip="Enter Todo")
+add_button = sg.Button("Add")
+
+window = sg.Window("My ToDo App", layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
