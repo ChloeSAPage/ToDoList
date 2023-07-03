e2659e9637b9a545cec1b0c47d15c09ec78df658
ChloeSPage <chloe.s.a.page@Gmail.com>
2023-07-03 14:15:18 +0100

    changed Functions to functions

--- cli.py
+++ cli.py
@@ -1,4 +1,4 @@
-import Functions
+import functions
 import time
 
 current_time = time.strftime("%b %d %Y %H:%S")
@@ -7,7 +7,7 @@ print(f"It is currently {current_time}")
 while True :
     user_action = input("Add, Show, Edit, Complete or Exit ToDo's: ")
     user_action =  user_action.strip().lower()
-    todo_list = Functions.get_todos()
+    todo_list = functions.get_todos()
 
     if user_action.startswith ("add"):
 
@@ -16,30 +16,30 @@ while True :
 
         todo_list.append(todo)
     
-        Functions.write_todos(todo_list)
+        functions.write_todos(todo_list)
     
     elif user_action.startswith ("show"):
-        Functions.show_todo(todo_list)
+        functions.show_todo(todo_list)
 
     elif user_action.startswith ("edit"):
         #"show" case here as well for user knowledge
-        Functions.show_todo(todo_list)
+        functions.show_todo(todo_list)
         try:
             user_action_split = user_action.split(" ", 1) 
             
             number = int(user_action_split[1]) - 1 
             
-            yesno = Functions.yes_no("edit", number, todo_list)
+            yesno = functions.yes_no("edit", number, todo_list)
             
             match yesno:
                 case "yes":
                     new_todo = input("Enter new ToDo: ") + "\n"
                     
-                    todo_list = Functions.get_todos()
+                    todo_list = functions.get_todos()
 
                     todo_list[number] = new_todo
                     
-                    Functions.write_todos(todo_list)
+                    functions.write_todos(todo_list)
 
                 case "no":
                         continue
@@ -49,22 +49,22 @@ while True :
             
     elif  user_action.startswith ("complete"):
         #"show" case for user knowledge
-        Functions.show_todo(todo_list)
+        functions.show_todo(todo_list)
         try:
             user_action_split = user_action.split(" ", 1) 
 
             number = int(user_action_split[1]) - 1
             todo_to_remove = todo_list[number].strip().capitalize()
             
-            yesno = Functions.yes_no("complete", number, todo_list)
+            yesno = functions.yes_no("complete", number, todo_list)
             
             match yesno:
                 case "yes":
-                    todo_list = Functions.get_todos()
+                    todo_list = functions.get_todos()
 
                     todo_list.pop(number)
 
-                    Functions.write_todos(todo_list)
+                    functions.write_todos(todo_list)
 
                     print(todo_to_remove, "was removed")
                 case "no":
