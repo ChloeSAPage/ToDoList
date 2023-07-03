e78cc8b5b53ad94fb5adef0a5e24ac210db60ca1
ChloeSPage <chloe.s.a.page@Gmail.com>
2023-07-03 14:15:00 +0100

    Added complete and edit button

--- GUI.py
+++ GUI.py
@@ -1,41 +1,55 @@
-import Functions
+import functions
 import PySimpleGUI as sg
 
 add_ = [[sg.Text("Type in a Todo")],
-        sg.InputText(tooltip="Enter Todo", key = "new_todo"),
+        sg.InputText(tooltip="Enter Todo", key = "todo"),
         sg.Button("Add")]
 
-list_box = sg.Listbox(values=Functions.get_todos(), key = "todo_list", 
+list_box = sg.Listbox(values=functions.get_todos(), key = "todo_list", 
             enable_events=True,size = [44, 10])
 
 edit_ = sg.Button("Edit")
 
+complete_ = sg.Button("Complete")
+
+exit_ = [sg.Button("Exit")]
+
 
 window = sg.Window("My ToDo App",
                    layout=[add_,
-                            [list_box, edit_]], font=("Helvetica", 12))
+                    [list_box, edit_ , complete_], exit_], 
+                    font=("Comic Sans", 12))
 
 while True:
-    button, user_input = window.read()
+    button, values = window.read()
     print(button)
-    print(user_input)
+    print(values)
     match button:
         case "Add":
-            todo_list = Functions.get_todos()
-            new_todo = user_input["new_todo"] +"\n"
+            todo_list = functions.get_todos()
+            new_todo = values["todo"] +"\n"
             todo_list.append(new_todo.capitalize())
-            Functions.write_todos(todo_list)
+            functions.write_todos(todo_list)
             window["todo_list"].update(values=todo_list)
         case "Edit":
-            todo_to_edit = user_input["todo_list"][0]
-            new_todo = user_input["new_todo"] +"\n"
-            todo_list = Functions.get_todos()
+            todo_to_edit = values["todo_list"][0]
+            new_todo = values["todo"] +"\n"
+            todo_list = functions.get_todos()
             index = todo_list.index(todo_to_edit)
             todo_list[index] = new_todo.capitalize()
-            Functions.write_todos(todo_list)
+            functions.write_todos(todo_list)
             window["todo_list"].update(values=todo_list)
+        case "Complete":
+            todo_to_complete = values["todo_list"][0]
+            todo_list = functions.get_todos()
+            todo_list.remove(todo_to_complete)
+            functions.write_todos(todo_list)
+            window["todo_list"].update(values=todo_list)
+            window["todo"].update(value="")
+        case "Exit":
+            break
         case "todo_list":
-            window["new_todo"].update(value=user_input["todo_list"][0])
+            window["todo"].update(value=values["todo_list"][0])
         case sg.WIN_CLOSED:
           break
 
