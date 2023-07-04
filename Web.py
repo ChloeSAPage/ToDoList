052e1bf08f72ef24104c37600fe64bcc4a3038d8
ChloeSPage <chloe.s.a.page@Gmail.com>
2023-07-04 16:50:56 +0100

    Implemented Add ToDo

--- Web.py
+++ Web.py
@@ -3,12 +3,21 @@ import functions
 
 todo_list = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todo_list.append(todo)
+    functions.write_todos(todo_list)
+
+
+
 st.title("My ToDo App")
 st.subheader("this is my todo app")
 st.write("tell Alex I love him")
 
 for todo in todo_list:
-    st.checkbox(todo)
+    st.checkbox(todo, key=todo)
 
-st.text_input(label="new todo", placeholder=("Add a new ToDo..."), 
-              label_visibility="collapsed")
+st.text_input(label="Add a new todo", placeholder=("Add a new ToDo..."), 
+              label_visibility="collapsed", on_change=add_todo, key="new_todo")
+
+st.session_state
